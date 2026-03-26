6f03ec1806570655e34ff6e02f0dd4c2b3308af8
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-03-26 22:34:01 +0530

    UC2: Added all HTTP methods with ResponseEntity

--- src/main/java/com/example/addressbook/controller/AddressBookController.java
+++ src/main/java/com/example/addressbook/controller/AddressBookController.java
@@ -1,5 +1,6 @@
 package com.example.addressbook.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,7 +8,27 @@ import org.springframework.web.bind.annotation.*;
 public class AddressBookController {
 
     @GetMapping
-    public String getMessage() {
-        return "Address Book App is Working!";
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("Get All Contacts");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getById(@PathVariable int id) {
+        return ResponseEntity.ok("Get Contact ID: " + id);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create() {
+        return ResponseEntity.ok("Contact Created");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@PathVariable int id) {
+        return ResponseEntity.ok("Updated ID: " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        return ResponseEntity.ok("Deleted ID: " + id);
     }
 }
